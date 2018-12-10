c98683913e23c8a557bfdfc50cd9ed3d590b93fe
Chinmay Gangal <chinmay2312@gmail.com>
2018-12-10 06:38:59 -0600

    - time slots dropdown - disabled options

--- app/src/main/java/com/chinmayg/hacks/shopsmart/CheckoutActivity.java
+++ app/src/main/java/com/chinmayg/hacks/shopsmart/CheckoutActivity.java
@@ -2,15 +2,24 @@ package com.chinmayg.hacks.shopsmart;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CheckoutActivity extends Activity {
 	
 	TextView checkoutTotal,tv_shopName,tv_shopAddr;
 	Button placeOrder;
+	Spinner timeSlots;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +39,40 @@ public class CheckoutActivity extends Activity {
 		tv_shopAddr = findViewById(R.id.tv_checkout_shopaddr);
 		tv_shopAddr.setText(shopAddr);
 		
+		timeSlots = findViewById(R.id.time_slots);
+		ArrayList<String> timeSlotsArrl = new ArrayList<>();
+		timeSlotsArrl.add("");
+		timeSlotsArrl.add("10am - 12pm");
+		timeSlotsArrl.add("12pm - 2pm");
+		timeSlotsArrl.add("2pm - 4pm");
+		timeSlotsArrl.add("4pm - 6pm");
+		ArrayAdapter<String> timeSlotAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, timeSlotsArrl){
+			@Override
+			public boolean isEnabled(int position) {
+				return (position>=2);
+			}
+			
+			@Override
+			public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+				View v = super.getDropDownView(position, convertView, parent);
+				TextView tv = (TextView)v;
+				tv.setTextColor(Color.GRAY);
+				if(position>=2)
+					tv.setTextColor(Color.BLACK);
+				
+				return v;
+			}
+		};
+		timeSlots.setAdapter(timeSlotAdapter);
+		
 		placeOrder = findViewById(R.id.btn_place_order);
 		placeOrder.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 			
 			}
+			
+			
 		});
 		
 	}
