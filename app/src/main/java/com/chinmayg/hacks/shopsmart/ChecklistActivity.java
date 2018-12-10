b6224ac9ba8236a7f23899b78e7528ce8efb3e74
Chinmay Gangal <chinmay2312@gmail.com>
2018-12-09 19:51:59 -0600

    - removed useless imports - avoid hardcoded strings

--- app/src/main/java/com/chinmayg/hacks/shopsmart/ChecklistActivity.java
+++ app/src/main/java/com/chinmayg/hacks/shopsmart/ChecklistActivity.java
@@ -2,21 +2,16 @@ package com.chinmayg.hacks.shopsmart;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ChecklistActivity extends Activity {
 	
